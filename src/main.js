966fa64ef4ceaa9e2b90a5090e8fe3791fa91e86
Michelle Kim <mkim139@ucsc.edu>
2022-04-18 10:50:19 -0700

    parallax, json, and comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
+// SUBMISSION COMMENTS
+//
+// Michelle Kim
+// Rocket Patrol Mod - Pollen Patrol
+// 4/18/22
+//
+// took 12 hours over 2 days
+//
+// the Player starts out with 50 points, hitting a bee will dock 10 points
+// and hitting the fly will add 50 points, no effect on timer
+//
+// Point breakdown:                 Made new music/sound                 +5
+//                                  Created new scrolling background     +5
+//                                  Display time left                   +10
+//                                  Create new title screen             +10
+//                                  Parallax scrolling                  +10
+//                                  New spaceship type                  +20
+//                                  New Artwork for assets              +20
+//                                  Mouse control                       +20
+//                                                              total:  100
+
+// Note: sometimes the rocket will stick to the x=0 pos, in that case
+//       please just reload the browser, it should work fine
+
+
 
 let config = {
     type: Phaser.AUTO,
